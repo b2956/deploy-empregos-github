ba23c2d1ed411f46a48cbee9e952cb2da4b035ae
Bruno Fávero <bruno.2956@gmail.com>
2020-07-21 16:40:06 -0300

    depploying to gh pages

---
+++ precache-manifest.9a02f95297980712475d416c30dc4eed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f216b4c39162c20c2bfce344940930cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0241ba6c06a3f32e10f",
+    "url": "/static/js/2.e1ba9852.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/static/js/2.e1ba9852.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a56eac629ec6ea8fe913",
+    "url": "/static/js/main.250f7727.chunk.js"
+  },
+  {
+    "revision": "ceae34549e428130a3bf",
+    "url": "/static/js/runtime-main.af49940b.js"
+  }
+]);
